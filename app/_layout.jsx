7a0b6a2772c689fc98061e3dd3842c443a9840d9
sameerkindia <sameerkhanaa5@gmail.com>
2025-09-12 08:56:44 +0530

    home screen and layout

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,5 +1,24 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "react-native";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <Stack
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#fff",
+          },
+          headerTintColor: "#000",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+        }}
+      >
+        <Stack.Screen name="index" />
+        {/* <Stack.Screen name="overview/[id].jsx" /> */}
+      </Stack>
+    </>
+  );
 }
--- app/index.jsx
+++ app/index.jsx
@@ -1,7 +1,25 @@
-import CategoriesScreen from "../screens/CategoriesScreen";
+import { FlatList } from "react-native";
+import CategoryGridTile from "../components/CategoryGridTile";
+import { CATEGORIES } from "../data/DummyData";
+
+export const options = {
+  title: "Home",
+};
+
+function renderCategoryItem(itemData) {
+  return (
+    <CategoryGridTile title={itemData.item.title} color={itemData.item.color} id={itemData.item.id} />
+  );
+}
 
 export default function Index() {
+  console.log(CATEGORIES[0])
   return (
-    <CategoriesScreen />
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={(item) => item.id}
+      renderItem={renderCategoryItem}
+      numColumns={2}
+    />
   );
 }
