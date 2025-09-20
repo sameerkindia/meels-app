d085a1e2f63def619c8b15cc47f3589688ddeb81
sameerkindia <sameerkhanaa5@gmail.com>
2025-09-20 16:01:13 +0530

    home screen

---
+++ app/(home)/index.jsx
@@ -0,0 +1,24 @@
+import { FlatList } from "react-native";
+import CategoryGridTile from "../../components/CategoryGridTile";
+import { CATEGORIES } from "../../data/DummyData";
+
+export const options = {
+  title: "Home",
+};
+
+function renderCategoryItem(itemData) {
+  return (
+    <CategoryGridTile title={itemData.item.title} color={itemData.item.color} id={itemData.item.id} />
+  );
+}
+
+export default function Index() {
+  return (
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={(item) => item.id}
+      renderItem={renderCategoryItem}
+      numColumns={2}
+    />
+  );
+}
